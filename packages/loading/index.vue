2b8e51e90f17b542953f165fd183af2887926543
zhan-hc <zhan19527@gmail.com>
2022-06-29 00:00:34 +0800

    feat: loading组件增加music类型

---
+++ packages/loading/index.vue
@@ -0,0 +1,98 @@
+<template>
+  <div class="wym-loading--wrap">
+    <!-- 音乐律动 -->
+    <div
+      v-if="type === 'music'"
+      class="wym-loading_music"
+      :style="{ top: loadingPosition[position] }"
+    >
+      <ul>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+      </ul>
+      <span>正在加载...</span>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+  import { defineComponent, reactive, toRefs, watch } from 'vue'
+  export default defineComponent({
+    name: 'Loading',
+    props: {
+      type: {
+        type: String,
+        default: 'music',
+      },
+      position: {
+        type: String,
+        default: 'center',
+      },
+    },
+    components: {},
+    setup() {
+      enum loadingPosition {
+        top = '20%',
+        bottom = '80%',
+        center = '50%',
+      }
+      return {
+        loadingPosition,
+      }
+    },
+  })
+</script>
+
+<style scoped lang="scss">
+  @import '@/assets/style/_1px.scss';
+  @keyframes music {
+    0% {
+      transform: scaleY(1);
+    }
+    50% {
+      transform: scaleY(0.2);
+    }
+    100% {
+      transform: scaleY(1);
+    }
+  }
+  .wym-loading--wrap {
+    .wym-loading_music {
+      position: fixed;
+      top: 50%;
+      transform: translateY(-50%);
+      left: 0;
+      right: 0;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      ul {
+        padding: 0;
+        li {
+          display: inline-block;
+          width: 3px;
+          margin: 0 3px;
+          height: 20px;
+          list-style: none;
+          background-color: #ff5a4c;
+          transform-origin: center center;
+          animation: music 1.5s 0ms infinite ease-in-out;
+          &:nth-child(2) {
+            animation-delay: 0.3s;
+          }
+          &:nth-child(3) {
+            // animation-delay: 0.2s;
+          }
+          &:nth-child(4) {
+            animation-delay: 0.5s;
+          }
+        }
+      }
+      span {
+        font-size: 12px;
+      }
+    }
+  }
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,34 +5,38 @@
       src="../assets/logo.png"
       @click="clickOpenFunComponent"
     />
+    <Loading />
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
-import dialog from "../../packages/dialog";
+  import { defineComponent } from 'vue'
+  import Loading from '../../packages/loading/index.vue'
+  import dialog from '../../packages/dialog'
 
-export default defineComponent({
-  name: "Home",
-  components: {},
-  setup() {
-    // const {
-    //   appContext: {
-    //     config: { globalProperties },
-    //   },
-    // } = getCurrentInstance() as ComponentInternalInstance
-    const clickOpenFunComponent = () => {
-      dialog({})
-        .then(() => {
-          console.log("点击确认");
-        })
-        .catch(() => {
-          console.log("点击取消");
-        });
-    };
-    return {
-      clickOpenFunComponent,
-    };
-  },
-});
+  export default defineComponent({
+    name: 'Home',
+    components: {
+      Loading,
+    },
+    setup() {
+      // const {
+      //   appContext: {
+      //     config: { globalProperties },
+      //   },
+      // } = getCurrentInstance() as ComponentInternalInstance
+      const clickOpenFunComponent = () => {
+        dialog({})
+          .then(() => {
+            console.log('点击确认')
+          })
+          .catch(() => {
+            console.log('点击取消')
+          })
+      }
+      return {
+        clickOpenFunComponent,
+      }
+    },
+  })
 </script>
